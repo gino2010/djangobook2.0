f76d69b18a0ebc9a70258e86cf4d4ddbfebd815a
Gino Zhao <ginozhao2010@gmail.com>
2014-02-19 15:55:25 +0800

    chapter 11 Generic Views In 1.6, old list_detail has been deprecated, please reference https://docs.djangoproject.com/en/1.6/ref/class-based-views/generic-display/

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
 from books import views
+from books.models import Publisher
+from django.views.generic.list import ListView
 
 __author__ = 'Gino'
 
@@ -9,4 +11,8 @@ urlpatterns = patterns('',
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^thanks/$', views.thanks, name='thanks'),
     url(r'^author/$', views.show_author, name='show_author'),
+    # chapter11 Generic Views list_detail has been deprecated
+    url(r'^publishers/$', ListView.as_view(queryset= Publisher.objects.all(),
+                                           template_name='publisher_list_page.html',
+                                           object)),
 )
