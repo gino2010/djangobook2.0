9b97078866c1856b9fce52b28b7958e0096cb064
Gino Zhao <ginozhao2010@gmail.com>
2014-03-18 16:35:33 +0800

    chapter 14 user register and permission

--- usermanager/urls.py
+++ usermanager/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^$', views.user_home),
     url(r'^login/$', views.login_view, name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^register/$', views.register, name='register'),
 )
--- usermanager/views.py
+++ usermanager/views.py
@@ -1,4 +1,5 @@
 from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
@@ -26,3 +27,22 @@ def login_view(request):
 def logout_view(request):
     auth.logout(request)
     return HttpResponseRedirect("/user")
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/user")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {
+        'form': form,
+    })
+
+'''about user permission and group
+you can get user object by auth.authenticate() fuction, use user.user_permission.add() function
+to change user permission, then you can find the record in your database auth_user_user_permission table.
+other way, you can create permission group by Group model, and add permission in this group and assign this group
+to user by user.groups.add() function, then you can find this record in auth_user_groups table in your database.'''
